d70a2d15d5bdcf8f697fd5b5a7213a52239bd04d
Evian <icdk5b4zf@relay.firefox.com>
2023-10-09 11:21:28 +0200

    fix: do not display maintenance page to admins in front end.

--- Api/src/MetaGame/Controller/AdminController.php
+++ Api/src/MetaGame/Controller/AdminController.php
@@ -221,7 +221,7 @@ class AdminController extends AbstractFOSRestController
      */
     public function getMaintenanceStatus(): View
     {
-        $isGameInMaintenance = $this->adminService->isGameInMaintenance($this->getUser());
+        $isGameInMaintenance = $this->adminService->isGameInMaintenance();
 
         return $this->view(['gameInMaintenance' => $isGameInMaintenance], Response::HTTP_OK);
     }
@@ -239,7 +239,7 @@ class AdminController extends AbstractFOSRestController
     {
         $this->denyAccessIfNotAdmin();
 
-        if ($this->adminService->isGameInMaintenance($this->getUser())) {
+        if ($this->adminService->isGameInMaintenance()) {
             return $this->view('Game is already in maintenance', Response::HTTP_BAD_REQUEST);
         }
 
@@ -261,7 +261,7 @@ class AdminController extends AbstractFOSRestController
     {
         $this->denyAccessIfNotAdmin();
 
-        if (!$this->adminService->isGameInMaintenance($this->getUser())) {
+        if (!$this->adminService->$this->adminService->isGameInMaintenance()) {
             return $this->view('Game is not in maintenance', Response::HTTP_BAD_REQUEST);
         }
 
